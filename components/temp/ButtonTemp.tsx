3bf8a98a2f84d62b84d3ca5455f4cefc2573b749
ApMatheus <matheusaparecido101@gmail.com>
2024-04-07 19:50:44 -0300

    feat(Task 2) created

---
+++ components/temp/ButtonTemp.tsx
@@ -0,0 +1,17 @@
+import type { Temperature } from "apps/weather/loaders/temperature.ts";
+
+export interface Props {
+  temperature: Temperature | null;
+}
+
+export default function ButttonTemperature({ temperature }: Props) {
+  if (!temperature?.celsius) {
+    return null;
+  }
+
+  return (
+    <div class="fixed bottom-10 right-5 bg-blue-500 rounded-full p-4 w-fit h-[50px] font-bold text-white">
+      {temperature?.celsius + "ºC"}
+    </div>
+  );
+}
